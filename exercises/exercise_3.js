84d56e9fa54b7f1b308d022d5e2e0eceff68f7ad
Dan Rees <danrees8743@gmail.com>
2022-09-27 20:27:04 +0100

    Solution[exercise_3]: solution for excercise 3

--- exercises/exercise_3.js
+++ exercises/exercise_3.js
@@ -1,4 +1,4 @@
-const characters = require("../data/characters.json");
+const characters = require('../data/characters.json');
 
 // TODO: return only characters that
 // their height is between 180 and 200
@@ -6,8 +6,14 @@ const characters = require("../data/characters.json");
 const charactersWithMediumHeight = () => {
   // YOUR CODE HERE
   // YOU CAN CHANGE ANY CODE INSIDE THE FUNCTION
-  const MIN_HEIGHT = 0;
-  const MAX_HEIGHT = 300;
+  const MIN_HEIGHT = 180;
+  const MAX_HEIGHT = 201;
+  return characters.filter(
+    (character) =>
+      character.height !== 'unknown' &&
+      character.height >= MIN_HEIGHT &&
+      character.height <= MAX_HEIGHT
+  );
 };
 
 module.exports = charactersWithMediumHeight;
